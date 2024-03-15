60710ed8f1103a03df4a440d44f1ecada1b03271
Svetlana Yashchenko <svetikoduvanchik@gmail.com>
2024-03-15 22:21:56 +0200

    task correction and connection

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,5 @@
 function getElementWidth(content, padding, border) {
-    const getContent = Number.parseFloat(content);
-    const getPadding = Number.parseFloat(padding);
-    const getBorder = Number.parseFloat(border);
-    return getContent + getPadding * 2 + getBorder * 2;
+    return parseFloat(content) + parseFloat(padding)*2 + parseFloat(border)*2;
 }
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px")); 
