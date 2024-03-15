d892bec6cad883891e40b050b078008069f3e9fe
Svetlana Yashchenko <svetikoduvanchik@gmail.com>
2024-03-15 22:08:56 +0200

    task tree

---
+++ js/task-3.js
@@ -0,0 +1,9 @@
+function getElementWidth(content, padding, border) {
+    const getContent = Number.parseFloat(content);
+    const getPadding = Number.parseFloat(padding);
+    const getBorder = Number.parseFloat(border);
+    return getContent + getPadding * 2 + getBorder * 2;
+}
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px")); 
+console.log(getElementWidth("200px", "0px", "0px")); 
